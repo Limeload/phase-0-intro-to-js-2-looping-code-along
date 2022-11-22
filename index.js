207cad01166382862dc50356013e583c9e4531e4
Shraddha Rao <shraddharaom@gmail.com>
2022-11-21 22:01:24 -0600

    commit tree a786871d1abac7cfd9115ebf97e2d31d13a44977

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "suprise";
+
+function writeCards(names, event) {
+    let i = 0;
+    let messagesArray = [];
+    while ( i < names.length) {
+        let message = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        i++;
+        messagesArray.push(message);
+        debugger;
+    }
+    return messagesArray;
+}
+
+function countDown(counting) {
+    let i = 10
+    while (i > -1) {
+        console.log(i);
+        i--
+        debugger;
+    }
+    return counting;
+}
